92c2c69dfd34f0a531fa4cb95acf671eff77a67a
DAV-IDE <davidef2017@gmail.com>
2024-04-23 18:12:19 +0200

    Initial Commit

--- app/src/main/java/it/uninsubria/ricette/RegistrazioneActivity2.kt
+++ app/src/main/java/it/uninsubria/ricette/RegistrazioneActivity2.kt
@@ -1,34 +1,20 @@
 package it.uninsubria.ricette
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
-
+class RegistrazioneActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_registrazione2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-            // ciao professore tutto bene?
-            //bene
-        }*/
-
-        button = findViewById(R.id.button1)
-
-        button.setOnClickListener {
-            val intent = Intent (this@MainActivity, RegistrazioneActivity2 :: class.java)
-            startActivity(intent)
         }
     }
 }
