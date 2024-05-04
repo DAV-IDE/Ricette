b73a72e2eb36de85687d07c545f05f503cb19bc3
DAV-IDE <davidef2017@gmail.com>
2024-05-04 11:42:19 +0200

    layout SceltaActivity

--- app/src/main/java/it/uninsubria/ricette/SceltaActivity.kt
+++ app/src/main/java/it/uninsubria/ricette/SceltaActivity.kt
@@ -1,5 +1,6 @@
 package it.uninsubria.ricette
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SceltaActivity : AppCompatActivity() {
+
+    private lateinit var buttonCerca : Button
+    private lateinit var buttonCrea : Button
 
 
 
@@ -19,6 +23,19 @@ class SceltaActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        buttonCerca = findViewById(R.id.buttonSceltaCerca)
+        buttonCrea = findViewById(R.id.buttonSceltaCrea)
+
+        buttonCerca.setOnClickListener {
+            val intent = Intent (this@SceltaActivity, CercaRicetteActivity :: class.java)
+            startActivity(intent)
+        }
+
+        buttonCrea.setOnClickListener {
+            val intent = Intent (this@SceltaActivity, CreaRicetteActivity :: class.java)
+            startActivity(intent)
         }
     }
 }
