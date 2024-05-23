17a93de755dbedff70be3e216c83c0c5a2a80060
DAV-IDE <davidef2017@gmail.com>
2024-05-23 13:36:56 +0200

    layout SceltaActivity

--- app/src/main/java/it/uninsubria/ricette/CercaRicetteActivity.kt
+++ app/src/main/java/it/uninsubria/ricette/CercaRicetteActivity.kt
@@ -1,17 +1,21 @@
 package it.uninsubria.ricette
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 import android.widget.ListView
+import android.widget.SearchView
 
 
 class CercaRicetteActivity : AppCompatActivity() {
 
     private lateinit var listView: ListView
+    private lateinit var ingredients : ArrayList<String>
+    private lateinit var searchView : SearchView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +28,23 @@ class CercaRicetteActivity : AppCompatActivity() {
             insets
         }
         listView = findViewById(R.id.multiple_list_view)
-        val ingredients = arrayListOf("Acqua", "Aceto", "Farina")
-        val adapter = CustomAdapter(this, ingredients)
+        searchView = findViewById(R.id.searchView)
+        ingredients = arrayListOf("Acqua", "Aceto", "Farina")
+        val adapter : ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1, ingredients)
         listView.adapter = adapter
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+
+        })
+       // listView.setOnItemClickListener{ }
 
     }
 }
