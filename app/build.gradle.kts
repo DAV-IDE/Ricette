c1122bb1fad18103c2b0a42e3ad88ce803be3525
DAV-IDE <davidef2017@gmail.com>
2024-05-24 21:23:29 +0200

    layout SceltaActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.googleGmsGoogleServices)
 }
 
 android {
@@ -9,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "it.uninsubria.ricette"
-        minSdk = 19
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -37,11 +38,19 @@ android {
 
 dependencies {
 
+
+
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+
+    // Add the dependency for the Firebase SDK for Google Analytics
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/it/uninsubria/ricette/MainActivity.kt
+++ app/src/main/java/it/uninsubria/ricette/MainActivity.kt
@@ -2,15 +2,22 @@ package it.uninsubria.ricette
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonRegistrazione: Button
     private lateinit var buttonAccedi: Button
+    private lateinit var buttonProva: Button
+    private lateinit var firebaseRef : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         buttonRegistrazione = findViewById(R.id.buttonMainR)
         buttonAccedi = findViewById(R.id.buttonMainA)
+        buttonProva = findViewById(R.id.buttonProva)
 
         buttonRegistrazione.setOnClickListener {
             val intent = Intent (this@MainActivity, RegistrazioneActivity2 :: class.java)
@@ -36,4 +44,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun prova(view : View){
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+       myRef.setValue("Hello, World!")
+
+
+    }
+
 }
