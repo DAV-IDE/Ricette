d6a770c6f99d4bfa875a0772f9334f26681597f7
SofIervolino <sofiaiervolino90@gmail.com>
2024-05-26 10:35:37 +0200

    Aggiunti ingredienti ListView

--- app/src/main/java/it/uninsubria/ricette/CercaRicetteActivity.kt
+++ app/src/main/java/it/uninsubria/ricette/CercaRicetteActivity.kt
@@ -29,7 +29,28 @@ class CercaRicetteActivity : AppCompatActivity() {
         }
         listView = findViewById(R.id.multiple_list_view)
         searchView = findViewById(R.id.searchView)
-        ingredients = arrayListOf("Acqua", "Aceto", "Farina")
+        ingredients = arrayListOf("aglio", "agnello", "albicocca", "alloro", "aloe", "ananas",
+                                    "anatra", "aneto", "arachidi", "arancia", "asparagi",
+                                    "avocado", "basilico", "biancospino", "broccoli", "burro",
+                                    "cacao", "cannella", "cardamomo", "carciofi", "carne di manzo",
+                                    "carote", "cavolfiore", "cavolo", "cavolo riccio", "cedro",
+                                    "cetriolo", "chiodi di garofano", "cipolla", "coriandolo",
+                                    "couscous", "cumino", "curcuma", "datteri", "dragoncello",
+                                    "fagioli", "fagiolini", "farina", "fegato", "finocchio",
+                                    "formaggio", "fragola", "frumento", "fungi", "gelato",
+                                    "girasole", "granchio", "insalata", "kiwi", "lampone",
+                                    "latte", "lattuga", "limone", "litchi", "maiale", "mais",
+                                    "mandarino", "mango", "melanzana", "melone", "menta", "merluzzo",
+                                    "miele", "mirtilli", "nocciola", "noce", "noce moscata", "oliva",
+                                    "origano", "orzo", "pane", "panna", "papaya", "paprika", "pasta",
+                                    "patata", "pepe", "pepe bianco", "pepe di Cayenna", "pepe nero",
+                                    "pepe rosa", "peperoncino", "peperoncino in polvere", "peperoni",
+                                    "pesca", "pesce spada", "petto di pollo", "piadina", "pistacchio",
+                                    "polenta", "pollo", "pomodori", "prezzemolo", "prosciutto", "prugna",
+                                    "quinoa", "radicchio", "riso", "rosmarino", "salmone", "sale", "salsiccia",
+                                    "sedano", "semi di papavero", "sesamo", "spinaci", "tacchino",
+                                    "timo", "tonno", "uva", "uva passa", "vaniglia", "verza", "vongole",
+                                    "yogurt", "zafferano", "zenzero", "zucca", "zucchero", "zucchine")
         val adapter : ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1, ingredients)
         listView.adapter = adapter
 
