defbbd176b4fa3bd8ec245c7f4779b57e7e55bcd
SofIervolino <sofiaiervolino90@gmail.com>
2024-06-13 12:04:00 +0200

    collegamenti tra activity e creazione Ricette2

--- app/src/main/java/it/uninsubria/ricette/RicettaActivity2.kt
+++ app/src/main/java/it/uninsubria/ricette/RicettaActivity2.kt
@@ -1,27 +1,20 @@
 package it.uninsubria.ricette
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MieRicetteActivity : AppCompatActivity() {
+class RicettaActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_mie_ricette)
+        setContentView(R.layout.activity_ricetta2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val cardView = findViewById<CardView>(R.id.cardView2)
-        cardView.setOnClickListener {
-            val intent = Intent(this, RicettaActivity2::class.java)
-            startActivity(intent)
         }
     }
 }
