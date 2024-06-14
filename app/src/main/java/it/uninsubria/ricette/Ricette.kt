88d46bd2a877425a47a46142db9a3ca6cd5df37d
SofIervolino <sofiaiervolino90@gmail.com>
2024-06-14 20:34:54 +0200

    passaggio id

--- app/src/main/java/it/uninsubria/ricette/Ricette.kt
+++ app/src/main/java/it/uninsubria/ricette/Ricette.kt
@@ -1,9 +1,11 @@
 package it.uninsubria.ricette
 
 data class Ricette(
+    val id: String= "",
     val nome: String = "",
     val ingredienti: List<String> = emptyList(),
     val quantita: List<String> = emptyList(),
     val unita: List<String> = emptyList(),
-    val procedimento: String = ""
+    val procedimento: String = "",
+    val fotoUrl: String = ""
 )
