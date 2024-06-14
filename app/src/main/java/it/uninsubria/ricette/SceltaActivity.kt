d8c2e34e560d3056336cf61b2f3e55dab4e79b69
SofIervolino <sofiaiervolino90@gmail.com>
2024-06-14 20:32:52 +0200

    passaggio id

--- app/src/main/java/it/uninsubria/ricette/SceltaActivity.kt
+++ app/src/main/java/it/uninsubria/ricette/SceltaActivity.kt
@@ -3,13 +3,14 @@ package it.uninsubria.ricette
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SceltaActivity : AppCompatActivity() {
+
+    private var userId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +21,24 @@ class SceltaActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Recupera l'ID utente dall'intento
+        userId = intent.getStringExtra("USER_ID")
     }
 
     fun sceltaProfilo(view: View) {
-        val intent = Intent (this@SceltaActivity, ProfiloUtenteActivity :: class.java)
+        val intent = Intent(this@SceltaActivity, ProfiloUtenteActivity::class.java)
         startActivity(intent)
     }
+
     fun cerca(view: View) {
-        val intent = Intent (this@SceltaActivity, CercaRicetteActivity :: class.java)
+        val intent = Intent(this@SceltaActivity, CercaRicetteActivity::class.java)
         startActivity(intent)
     }
+
     fun crea(view: View) {
-        val intent = Intent (this@SceltaActivity, CreaRicetteActivity :: class.java)
+        val intent = Intent(this@SceltaActivity, CreaRicetteActivity::class.java)
+        intent.putExtra("USER_ID", userId)
         startActivity(intent)
     }
 }
