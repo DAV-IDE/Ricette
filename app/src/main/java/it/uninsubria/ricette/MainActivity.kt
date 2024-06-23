a85f6b3f825a50128730260ac938916018ca5074
SofIervolino <sofiaiervolino90@gmail.com>
2024-06-23 22:35:56 +0200

    tolto librerie non usate

--- app/src/main/java/it/uninsubria/ricette/MainActivity.kt
+++ app/src/main/java/it/uninsubria/ricette/MainActivity.kt
@@ -3,15 +3,10 @@ package it.uninsubria.ricette
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.Firebase
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
 
