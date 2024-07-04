c14e819460ed2e0c2c61c5a7f7d11daa22ab2865
SofIervolino <sofiaiervolino90@gmail.com>
2024-07-04 12:52:15 +0200

    Tolti import non utilizzati

--- app/src/main/java/it/uninsubria/ricette/MieRicetteActivity.kt
+++ app/src/main/java/it/uninsubria/ricette/MieRicetteActivity.kt
@@ -3,7 +3,6 @@ package it.uninsubria.ricette
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageButton
